534734ee927b718cc43277c27dc4f0f86738f320
Jomana Mohammed <138962547+jomana-mohammed@users.noreply.github.com>
2024-10-01 12:32:33 +0300

    Update page.tsx

--- src/app/navHeader/page.tsx
+++ src/app/navHeader/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArchway } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
